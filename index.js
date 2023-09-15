b538eec91bc11c43b1958545db443ba0e71a9cc7
Ben Wagner <benwagner2013@gmail.com>
2023-09-14 20:37:37 -0500

    Initial commit of Node Syslog server impl

---
+++ index.js
@@ -0,0 +1,104 @@
+const Syslog = require('simple-syslog-server');
+const PropertiesReader = require('properties-reader');
+const winston = require('winston');
+const fs = require('fs');
+
+const logger = winston.createLogger({
+    level: 'info',
+    format: winston.format.prettyPrint(),
+    defaultMeta: { service: 'user-service' },
+    transports: [
+        new winston.transports.File({ filename: 'syslog-server.log'}),
+        new winston.transports.Console()
+    ],
+});
+
+const prop = PropertiesReader('syslog-server.properties')
+
+const socketType = prop.get('syslog.socketType');
+const address = prop.get('syslog.address') ;
+const port = prop.get('syslog.port') ;
+var server;
+
+switch(socketType){
+    case 'TCP':
+    case 'UDP':
+        server = Syslog(socketType);
+        break;
+    case 'TLS':
+        const tls_options = {
+            key: fs.readFileSync(prop.get('syslog.tls.privateKey')).toString(),
+            cert: fs.readFileSync(prop.get('syslog.tls.certificate')).toString(),
+            ca: [fs.readFileSync(prop.get('syslog.tls.ca')).toString()]
+        } ;
+        server = Syslog.TLS(tls_options);
+        break;
+    default:
+        throw 'Invalid socket type '+socketType;
+}
+ 
+// State Information
+var listening = false ;
+var clients = [] ;
+var count = 0 ;
+ 
+server.on('msg', data => {
+    console.info('message received (%i) from %s:%i\n%o\n', ++count, data.address, data.port, data) ;
+    /*
+    message received (1) from ::ffff:192.168.1.13:59666
+    {
+      "facility": "daemon",
+      "facilityCode": 3,
+      "severity": "info",
+      "severityCode": 6,
+      "tag": "systemd[1]",
+      "timestamp": "2018-12-26T17:53:57.000Z",
+      "hostname": "localhost",
+      "address": "::ffff:192.168.1.13",
+      "family": "IPv6",
+      "port": 20514,
+      "size": 80,
+      "msg": "Started Daily apt download activities."
+    }	
+    */
+})
+.on('invalid', err => {
+    console.warn('Invalid message format received: %o\n', err) ;
+})
+.on('error', err => {
+    console.warn('Client disconnected abruptly: %o\n', err) ;
+})
+.on('connection', s => {
+    let addr = s.address().address ;
+    console.info(`Client connected: ${addr}\n`) ;
+    clients.push(s) ;
+    s.on('data', (buff) => {
+        console.info(`data: ${buff.toString()}`);
+    })
+    s.on('end', () => {
+        console.info(`Client disconnected: ${addr}\n`) ;
+        let i = clients.indexOf(s) ;
+        if(i !== -1)
+            clients.splice(i, 1) ;
+    }) ;
+})
+.listen({host: address, port: port})
+.then(() => {
+    listening = true ;
+    console.info(socketType + ` now listening on: ${address}:${port}`) ;
+})
+.catch(err => {
+    if ((err.code == 'EACCES') && (port < 1024)) {
+        console.error('Cannot listen on ports below 1024 without root permissions. Select a higher port number: %o', err) ;
+    }
+    else { // Some other error so attempt to close server socket
+        console.error(`Error listening to ${address}:${port} - %o`, err) ;
+        try {
+            if(listening)
+                server.close() ;
+        }
+        catch (err) {
+            console.warn(`Error trying to close server socket ${address}:${port} - %o`, err) ;
+        }
+    }
+}) ;
